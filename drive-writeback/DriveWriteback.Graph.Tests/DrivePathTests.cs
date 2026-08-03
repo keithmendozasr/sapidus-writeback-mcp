97a97aa970756b073b01f992b3e3eb2e74a68988
Keith Mendoza <576988+keithmendozasr@users.noreply.github.com>
2026-08-03 06:57:51 -0700

    test(drive-writeback): add DrivePath.Normalize test cases

--- drive-writeback/DriveWriteback.Graph.Tests/DrivePathTests.cs
+++ drive-writeback/DriveWriteback.Graph.Tests/DrivePathTests.cs
@@ -4,4 +4,13 @@ namespace DriveWriteback.Graph.Tests;
 [Category("Unit")]
 public class DrivePathTests
 {
+    [TestCase("", "")]
+    [TestCase("/", "")]
+    [TestCase("root", "")]
+    [TestCase("/a/b/", "a/b")]
+    [TestCase("a/b", "a/b")]
+    public void Normalize_collapses_root_aliases_and_trims_slashes(string path, string expected)
+    {
+        Assert.That(DrivePath.Normalize(path), Is.EqualTo(expected));
+    }
 }

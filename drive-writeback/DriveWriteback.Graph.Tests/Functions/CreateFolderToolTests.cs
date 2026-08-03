d0c93dda07f7a25853a3f9d496169b68a42b4c35
Keith Mendoza <576988+keithmendozasr@users.noreply.github.com>
2026-08-03 07:42:00 -0700

    test(drive-writeback): add CreateFolderTool dry-run would-create test case

--- drive-writeback/DriveWriteback.Graph.Tests/Functions/CreateFolderToolTests.cs
+++ drive-writeback/DriveWriteback.Graph.Tests/Functions/CreateFolderToolTests.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using System.Text;
 using Microsoft.Graph;
 using Microsoft.Kiota.Abstractions.Authentication;
 using DriveWriteback.Functions;
@@ -18,5 +20,30 @@ public class CreateFolderToolTests
         var writeService = new DriveWriteService(client, options, new RecordingLogger<DriveWriteService>());
 
         return new CreateFolderTool(writeService);
+    }
+
+    [Test]
+    public async Task RunAsync_dry_run_reports_the_would_be_created_segment_and_makes_no_mutating_call()
+    {
+        var handler = new StubHttpMessageHandler(request =>
+        {
+            if (request.Method != HttpMethod.Get)
+                throw new InvalidOperationException($"Dry-run must never send a {request.Method} request.");
+
+            return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent("""{"value":[]}""", Encoding.UTF8, "application/json"),
+            });
+        });
+        var tool = CreateTool(handler, DriveWriteOptions.Default);
+
+        var result = await tool.RunAsync(null!, "new-folder", driveId: "drive-id");
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Does.StartWith("[DRY RUN]"));
+            Assert.That(result, Does.Contain("new-folder"));
+            Assert.That(result, Does.Contain("Not created"));
+        });
     }
 }

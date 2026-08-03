fc2f06d33b6e98105368004204a2c7ec6b76b935
Keith Mendoza <576988+keithmendozasr@users.noreply.github.com>
2026-08-03 07:45:22 -0700

    test(drive-writeback): add CreateFileToolTests fixture skeleton

---
+++ drive-writeback/DriveWriteback.Graph.Tests/Functions/CreateFileToolTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.Graph;
+using Microsoft.Kiota.Abstractions.Authentication;
+using DriveWriteback.Functions;
+using DriveWriteback.Graph.Tests.TestSupport;
+using DriveWriteback.Graph.Writes;
+
+namespace DriveWriteback.Graph.Tests.Functions;
+
+[TestFixture]
+[Category("Unit")]
+public class CreateFileToolTests
+{
+    private static CreateFileTool CreateTool(StubHttpMessageHandler handler, DriveWriteOptions options)
+    {
+        var httpClient = new HttpClient(handler) { BaseAddress = new Uri("https://graph.microsoft.com/v1.0") };
+        var graphClient = new GraphServiceClient(httpClient, new AnonymousAuthenticationProvider());
+        var client = new DriveGraphClient(graphClient);
+        var writeService = new DriveWriteService(client, options, new RecordingLogger<DriveWriteService>());
+
+        return new CreateFileTool(writeService);
+    }
+}

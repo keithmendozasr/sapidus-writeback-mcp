86f76a3196ce89a5a8d6b0a752c6d6b8c50ef877
Keith Mendoza <576988+keithmendozasr@users.noreply.github.com>
2026-08-03 07:39:36 -0700

    test(drive-writeback): add GetItemTool folder-path-resolution test case

--- drive-writeback/DriveWriteback.Graph.Tests/Functions/GetItemToolTests.cs
+++ drive-writeback/DriveWriteback.Graph.Tests/Functions/GetItemToolTests.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using System.Text;
 using Microsoft.Graph;
 using Microsoft.Kiota.Abstractions.Authentication;
 using DriveWriteback.Functions;
@@ -16,5 +18,27 @@ public class GetItemToolTests
         var graphClient = new GraphServiceClient(httpClient, new AnonymousAuthenticationProvider());
 
         return new DriveGraphClient(graphClient);
+    }
+
+    [Test]
+    public async Task RunAsync_reports_folder_kind_and_that_the_input_was_resolved_as_a_path()
+    {
+        var handler = new StubHttpMessageHandler(_ => Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = new StringContent(
+                """{"id":"folder-id","name":"Reports","folder":{},"eTag":"\"e1\"","cTag":"\"c1\"","size":0,"webUrl":"https://example/Reports"}""",
+                Encoding.UTF8,
+                "application/json"),
+        }));
+        var tool = new GetItemTool(CreateClient(handler));
+
+        var result = await tool.RunAsync(null!, "Reports", driveId: "drive-id");
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Does.Contain("folder"));
+            Assert.That(result, Does.Contain("resolved \"Reports\" as path"));
+            Assert.That(result, Does.Contain("folder-id"));
+        });
     }
 }
